b2923ee0678a1c202611727daf388229e81faf44
Tiago Azevedo Borges <tabgyn@hotmail.com>
2015-07-22 15:46:24 -0300

    Add paginas de erro 401,404 e 500; Add paginas de acesso e solicitar nova senha

---
+++ AppConsig.Web.Gestor/Controllers/AcessoController.cs
@@ -0,0 +1,45 @@
+﻿using System.Web.Mvc;
+using AppConsig.Web.Gestor.Models;
+
+namespace AppConsig.Web.Gestor.Controllers
+{
+    [AllowAnonymous]
+    public class AcessoController : Controller
+    {
+        // GET: Acesso
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Acesso
+        [HttpPost]
+        public ActionResult Index(AcessoModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                
+            }
+
+            return View(model);
+        }
+
+        // GET: ReeviarSenha
+        public ActionResult NovaSenha()
+        {
+            return View();
+        }
+
+        // POST: ReeviarSenha
+        [HttpPost]
+        public ActionResult NovaSenha(NovaSenha model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ AppConsig.Web.Gestor/Views/Shared/Error/401.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AppConsig.Web.Gestor
+{
+    public partial class _401 : System.Web.UI.Page
+    {
+        
+    }
+}
