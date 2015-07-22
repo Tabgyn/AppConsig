9bcec85c4df400e007d1d01c3c93fc9e88220c93
Tiago Azevedo Borges <tabgyn@hotmail.com>
2015-07-22 18:23:09 -0300

    Sync Home

---
+++ AppConsig.Web.Gestor/Models/AcessoModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppConsig.Web.Gestor.Models
+{
+    public class AcessoModel
+    {
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        public string Senha { get; set; }
+    }
+
+    public class NovaSenhaModel
+    {
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+    }
+}
