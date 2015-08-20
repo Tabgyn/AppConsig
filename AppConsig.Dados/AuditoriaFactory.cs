b92f499f2ac9bf4bb9d48f0fedeb2543aace86b2
Tiago Azevedo Borges <tabgyn@hotmail.com>
2015-08-20 08:20:25 -0300

    Add AuditoriaFactory

---
+++ AppConsig.Dados/AuditoriaFactory.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using AppConsig.Entidades;
+
+namespace AppConsig.Dados
+{
+    public class AuditoriaFactory
+    {
+        private AppContexto _contexto;
+
+        public AuditoriaFactory(AppContexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public Auditoria GetAudit(DbEntityEntry entry)
+        {
+            Auditoria auditoria = new Auditoria
+            {
+                Usuario = Thread.CurrentPrincipal.Identity.Name,
+                TabelaNome = GetTableName(entry),
+                TabelaId = (long) GetKeyValue(entry),
+                DataCriacao = DateTime.Now
+            };
+
+            //entry is Added 
+            if (entry.State == EntityState.Added)
+            {
+                var newValues = new StringBuilder();
+                SetAddedProperties(entry, newValues);
+                auditoria.DadoNovo = newValues.ToString();
+                auditoria.Acao = AuditActions.I.ToString();
+            }
+            //entry in deleted
+            else if (entry.State == EntityState.Deleted)
+            {
+                var oldValues = new StringBuilder();
+                SetDeletedProperties(entry, oldValues);
+                auditoria.DadoAntigo = oldValues.ToString();
+                auditoria.Acao = AuditActions.D.ToString();
+            }
+            //entry is modified
+            else if (entry.State == EntityState.Modified)
+            {
+                var oldValues = new StringBuilder();
+                var newValues = new StringBuilder();
+                SetModifiedProperties(entry, oldValues, newValues);
+                auditoria.DadoAntigo = oldValues.ToString();
+                auditoria.DadoNovo = newValues.ToString();
+                auditoria.Acao = AuditActions.U.ToString();
+            }
+
+            return auditoria;
+        }
+
+        private void SetAddedProperties(DbEntityEntry entry, StringBuilder newData)
+        {
+            foreach (var propertyName in entry.CurrentValues.PropertyNames)
+            {
+                var newVal = entry.CurrentValues[propertyName];
+                if (newVal != null)
+                {
+                    newData.AppendFormat("{0}={1} || ", propertyName, newVal);
+                }
+            }
+            if (newData.Length > 0)
+                newData = newData.Remove(newData.Length - 3, 3);
+        }
+
+        private void SetDeletedProperties(DbEntityEntry entry, StringBuilder oldData)
+        {
+            DbPropertyValues dbValues = entry.GetDatabaseValues();
+            foreach (var propertyName in dbValues.PropertyNames)
+            {
+                var oldVal = dbValues[propertyName];
+                if (oldVal != null)
+                {
+                    oldData.AppendFormat("{0}={1} || ", propertyName, oldVal);
+                }
+            }
+            if (oldData.Length > 0)
+                oldData = oldData.Remove(oldData.Length - 3, 3);
+        }
+
+        private void SetModifiedProperties(DbEntityEntry entry, StringBuilder oldData, StringBuilder newData)
+        {
+            DbPropertyValues dbValues = entry.GetDatabaseValues();
+            foreach (var propertyName in entry.OriginalValues.PropertyNames)
+            {
+                var oldVal = dbValues[propertyName];
+                var newVal = entry.CurrentValues[propertyName];
+                if (oldVal != null && newVal != null && !Equals(oldVal, newVal))
+                {
+                    newData.AppendFormat("{0}={1} || ", propertyName, newVal);
+                    oldData.AppendFormat("{0}={1} || ", propertyName, oldVal);
+                }
+            }
+            if (oldData.Length > 0)
+                oldData = oldData.Remove(oldData.Length - 3, 3);
+            if (newData.Length > 0)
+                newData = newData.Remove(newData.Length - 3, 3);
+        }
+
+        public long? GetKeyValue(DbEntityEntry entry)
+        {
+            var objectStateEntry = ((IObjectContextAdapter)_contexto).ObjectContext.ObjectStateManager.GetObjectStateEntry(entry.Entity);
+            long id = 0;
+            if (objectStateEntry.EntityKey.EntityKeyValues != null)
+                id = Convert.ToInt64(objectStateEntry.EntityKey.EntityKeyValues[0].Value);
+
+            return id;
+        }
+
+        private string GetTableName(DbEntityEntry dbEntry)
+        {
+            TableAttribute tableAttr = dbEntry.Entity.GetType().GetCustomAttributes(typeof(TableAttribute), false).SingleOrDefault() as TableAttribute;
+            string tableName = tableAttr != null ? tableAttr.Name : dbEntry.Entity.GetType().Name;
+            return tableName;
+        }
+    }
+
+    public enum AuditActions
+    {
+        I,
+        U,
+        D
+    }
+}
---
+++ AppConsig.Entidades/Auditoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AppConsig.Comum;
+
+namespace AppConsig.Entidades
+{
+    public class Auditoria : Entidade<long>
+    {
+        public long TabelaId { get; set; }
+        public string TabelaNome { get; set; }
+        public string Usuario { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public string Acao { get; set; }
+        public string DadoAntigo { get; set; }
+        public string DadoNovo { get; set; }
+    }
+}
