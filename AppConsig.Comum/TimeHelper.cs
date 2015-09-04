6091ab187dfc957f1bef593fdb234421f3465d12
Tiago Azevedo Borges <tabgyn@hotmail.com>
2015-09-04 15:18:44 -0300

    Add TimeHelper Corrigido Demo de Visão geral

---
+++ AppConsig.Comum/TimeHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Globalization;
+
+namespace AppConsig.Comum
+{
+    public class TimeHelper
+    {
+        public static string ObterDiferencaTempo(DateTime reference)
+        {
+            var culture = new CultureInfo("pt-BR");
+            var now = DateTime.Now;
+            var dayReference = reference.Day;
+            var monthReference = reference.Month;
+            var yearReference = reference.Year;
+            var dayNow = now.Day;
+            var monthNow = now.Month;
+            var yearNow = now.Year;
+            var today = dayReference == dayNow && monthReference == monthNow && yearReference == yearNow;
+            var yesterday = dayReference == dayNow - 1 && monthReference == monthNow && yearReference == yearNow;
+            var diff = (int)now.Subtract(reference).TotalMinutes;
+
+            if (!today)
+                return yesterday
+                    // Ontem 4 de setembro
+                    ? $"Ontem {reference.ToString("%d")} de {culture.DateTimeFormat.GetMonthName(reference.Month)}"
+                    // 3 de setembro de 2015
+                    : $"{reference.ToString("%d")} de {culture.DateTimeFormat.GetMonthName(reference.Month)} de {reference.Year}";
+
+            // Hoje 5 de setembro
+            if (diff >= 12 * 60 && diff < 24 * 60)
+            {
+                return
+                    $"Hoje {reference.ToString("%d")} de {culture.DateTimeFormat.GetMonthName(reference.Month)}";
+            }
+
+            // 10 horas atrás
+            if (diff >= 2 * 60 && diff < 12 * 60)
+            {
+                return $"{diff / 60} horas atrás";
+            }
+
+            // 1 hora atrás
+            if (diff >= 60 && diff < 2 * 60)
+            {
+                return $"{diff / 60} hora atrás";
+            }
+
+            // 30 minutos atrás
+            if (diff < 60 && diff >= 2)
+            {
+                return $"{diff} minutos atrás";
+            }
+
+            // 1 minuto atrás
+            if (diff < 2 && diff > 0)
+            {
+                return $"{diff} minuto atrás";
+            }
+
+            // Agora
+            return "Agora";
+        }
+    }
+}
